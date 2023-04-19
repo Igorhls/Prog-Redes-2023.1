9966d244c3d1b571d1d33a8626698656067107e5
Aristeu-Dantas <aristeu_dantas@hotmail.com>
2023-04-19 20:00:08 -0300

    Atividade01

--- DumpLer.py
+++ DumpLer.py
@@ -1,5 +1,3 @@
-# Igor Henrique
-
 import dpkt
 import datetime
 
@@ -8,34 +6,34 @@ arq = "cap1.dump"
 with open(arq, 'rb') as f:
     pcap = dpkt.pcap.Reader(f)
 
-    inicio_p = None
-    fim_p = None
-    maior_p = 0
-    imcompleto_p = 0
+    inicia = None
+    termina = None
+    maior = 0
+    incompleto_p = 0
     tamanho_p = 0
     total_p = 0
 
     for timestamp, buf in pcap:
-        if inicio_p is None:
-            inicio_p = datetime.datetime.fromtimestamp(timestamp)
+        if inicia is None:
+            inicia = datetime.datetime.fromtimestamp(timestamp)
         fim_p = datetime.datetime.fromtimestamp(timestamp)
 
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
 
         if len(buf) < ip.len:
-            imcompleto_p += 1
+            incompleto_p += 1
 
-        if ip.len > maior_p:
-            maior_p = ip.len
+        if ip.len > maior:
+            maior = ip.len
 
         tamanho_p += ip.len
         total_p += 1
 
     media_p = tamanho_p / total_p
 
-    print("Captura iniciada em:", inicio_p)
-    print("Captura terminada em:", fim_p)
-    print("Tamanho do maior pacote capturado:", maior_p)
-    print("Número de pacotes capturados incompletos:", imcompleto_p)
-    print("Tamanho médio dos pacotes capturados:", media_p
+    print("A captura de pacotes inicia em {0} e termina em {1}."
+          "\nO maior pacote capturado é: {2}"
+          "\nOs pacotes não salvos são: {3}"
+          "\nA média dos tamanhos de pacotes é: {4}"
+          .format(inicia,termina,maior,incompleto_p,media_p))
